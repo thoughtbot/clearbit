dd038bee6d04c1566a7e493672ee8b4220fe244e
Bernerd Schaefer <bj.schaefer@gmail.com>
2016-02-26 14:48:51 -0800

    Add clearbit.Client
    Adds a clearbit package exporting a Client and a few Clearbit API
    endpoints for basic interaction with the API.

---
+++ client.go
@@ -0,0 +1,42 @@
+package clearbit
+
+import (
+	"net/http"
+	"net/url"
+)
+
+const (
+	ProspectorPersonSearchURL = "https://prospector.clearbit.com/v1/people/search"
+	StreamingCompanySearchURL = "https://company-stream.clearbit.com/v2/companies/find"
+	StreamingPersonSearchURL  = "https://person-stream.clearbit.com/v2/people/find"
+)
+
+type Client struct {
+	apiKey string
+
+	httpClient *http.Client
+}
+
+// NewClient initializes a Clearbit API client with the provided apiKey.
+func NewClient(apiKey string) *Client {
+	return &Client{
+		apiKey:     apiKey,
+		httpClient: http.DefaultClient,
+	}
+}
+
+// Get requests endpoint with the provided params
+// and the client's API key.
+//
+// The caller must close the returned response's Body.
+func (c *Client) Get(endpoint string, params url.Values) (*http.Response, error) {
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.SetBasicAuth(c.apiKey, "")
+	req.URL.RawQuery = params.Encode()
+
+	return c.httpClient.Do(req)
+}
